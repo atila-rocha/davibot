f58748d62eef4b634da30e40785c8a2d60adca6f
Átila Carvalho Rocha <atilacarvalhorocha@hotmail.com>
2026-05-09 19:45:00 -0300

    refactor: .env

--- davibot/config/config.exs
+++ davibot/config/config.exs
@@ -1,10 +1,13 @@
 import Config
-import Dotenvy
+#import Dotenvy
 
-source!([".env", System.get_env()])
+#source!([".env", System.get_env()])
 
 config :nostrum,
-  token: env!("DISCORD_TOKEN",:string!) # na teoria era pra ter uma virgula aqui?
-  api_rate_limit_mode::all_events
+  #token: env!("DISCORD_TOKEN", :string!),
+  gateway_intents: :all,
+  ffmpeg: false
+  #api_rate_limit_mode: :all_events
 
-config :logger, level :info
+#config :logger,
+#  level: :info
